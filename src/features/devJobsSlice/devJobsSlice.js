65a6e28583847d0f021dec14dd9d0883e9f00798
Piotr <Piotr.Gdula92@gmail.com>
2021-12-09 18:57:27 +0100

    devJobsApp - add concurrently to package.json, add first congiure redux/toolkit

---
+++ src/features/devJobsSlice/devJobsSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  devJobs: [],
+  loading: true,
+  failed: false,
+};
+
+const checkForError = (response) => {
+  if (!response.ok) throw Error("ERROR" + response.statusText);
+  return response.json();
+};
+
+export const getDevJobs = createAsyncThunk(
+  "devjobs/getDevjobs",
+  async (url) => {
+    return await fetch(url)
+      .then(checkForError)
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error("FETCH_ERR: ", error);
+        // state.failed = true;
+      });
+  }
+);
+
+export const devJobsSlice = createSlice({
+  name: "devJobsSelected",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getDevJobs.pending, (state) => {
+      state.failed = false;
+      state.loading = true;
+    });
+    builder.addCase(getDevJobs.fulfilled, (state, action) => {
+      state.loading = false;
+      state.failed = false;
+      state.devJobs = action.payload;
+    });
+    builder.addCase(getDevJobs.rejected, (state) => {
+      state.failed = true;
+    });
+  },
+});
