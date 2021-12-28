638f54f4f9d4ea30835dba32735f225efac06380
Piotr <Piotr.Gdula92@gmail.com>
2021-12-28 22:38:12 +0100

    add listOfJobs and reactRouter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Jobs } from "./components/jobs/Jobs";
+import { Job } from "./components/jobs/Job";
 import { Provider } from "react-redux";
 import store from "./features/store/store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-      <Jobs />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+        <Switch>
+          <Route exact path="/" component={Jobs} />
+          <Route exact path="/id/:id" component={Job} />
+        </Switch>
+        {/* <Jobs /> */}
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
