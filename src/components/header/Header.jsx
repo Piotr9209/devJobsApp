05a428a657e53c02b81acd6f72de38db2a99c865
Piotr <Piotr.Gdula92@gmail.com>
2022-01-10 22:50:11 +0100

    added css to project

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,1 +1,21 @@
 import React from "react";
+import { setToggleTheme } from "../../features/devJobsSlice/devJobsSlice";
+import "./header.scss";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div>
+          <p>devjobs</p>
+        </div>
+        <div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+      </div>
+    </header>
+  );
+};
