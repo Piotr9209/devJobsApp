2acdbec5b27e5bdf82e956195d286efc7165fb07
Piotr <Piotr.Gdula92@gmail.com>
2022-01-10 22:23:15 +0100

    conflict in index

--- src/index.js
+++ src/index.js
@@ -2,24 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Jobs } from "./components/jobs/Jobs";
-<<<<<<< HEAD
-import { Header } from "./components/header/Header.jsx";
-=======
 import { Job } from "./components/jobs/Job";
->>>>>>> showJobs
 import { Provider } from "react-redux";
 import store from "./features/store/store";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <Provider store={store}>
-      <Header />
-      <App />
-      <Jobs />
-    </Provider>
-=======
     <Router>
       <Provider store={store}>
         <App />
@@ -30,7 +19,6 @@ ReactDOM.render(
         </Switch>
       </Provider>
     </Router>
->>>>>>> showJobs
   </React.StrictMode>,
   document.getElementById("root")
 );
